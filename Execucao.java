f630f564096cbbb1b92490eff97b42cb6bd509b6
ViniciusPelizzari <vinipelizzari2410@gmail.com>
2023-03-03 15:46:22 -0300

    IF_ELSE

--- Execucao.java
+++ Execucao.java
@@ -1,14 +1,25 @@
+import java.util.Scanner;
+
 public class Execucao {
 
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("PF ou PJ: ");
+        String pfpj = s.nextLine();
+        System.out.println(" ");
 
-        PessoaJuridica pj = new PessoaJuridica();
-        System.out.println(" ");
-        System.out.println(pj);
+        if (pfpj.equals("PF")) {
 
-        System.out.println(" ");
-        PessoaFisica pf = new PessoaFisica();
-        System.out.println(" ");
-        System.out.println(pf);
+            PessoaFisica pf = new PessoaFisica();
+            System.out.println(" ");
+            System.out.println(pf);
+
+        } else if (pfpj.equals("PJ")) {
+
+            PessoaJuridica pj = new PessoaJuridica();
+            System.out.println(" ");
+            System.out.println(pj);
+
+        }
     }
 }
